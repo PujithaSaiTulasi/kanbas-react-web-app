21c5511bc0020d62910b1d39a4aa05256387e212
PujithaSaiTulasi <mps.tulasi@gmail.com>
2024-10-01 00:32:25 -0400

    kanbas 4.3 completed

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -4,87 +4,112 @@ export default function Dashboard() {
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link" to="/Kanbas/Courses/1234/Home">
-          <img src="/images/reactjs.jpg" width={200} />
-          <div>
-              <h5>CS1234 React JS</h5>
-              <p className="wd-dashboard-course-title"> Full Stack software developer </p>
-              <button> Go </button>
-          </div>
-          </Link>
-        </div> <br />
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link" to="/Kanbas/Courses/5610/Home">
-          <img src="/images/wd.jpg" width={200} />
-          <div>
-              <h5>CS5610 Web Development</h5>
-              <p className="wd-dashboard-course-title"> Web Application Development </p>
-              <button> Go </button>
-          </div>
-          </Link>
-        </div> <br />
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link" to="/Kanbas/Courses/5520/Home">
-          <img src="/images/mad.jpg" width={200} />
-          <div>
-              <h5>CS5520 Mobile Application Development</h5>
-              <p className="wd-dashboard-course-title"> Mobile Application Development </p>
-              <button> Go </button>
-          </div>
-          </Link>
-        </div> <br />
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link" to="/Kanbas/Courses/5220/Home">
-          <img src="/images/pm.jpg" width={200} />
-          <div>
-              <h5>EMGT5220 Engineering Project Management</h5>
-              <p className="wd-dashboard-course-title"> Engineering Project Management </p>
-              <button> Go </button>
-          </div>
-          </Link>
-        </div> <br />
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link" to="/Kanbas/Courses/6305/Home">
-          <img src="/images/fm.jpg" width={200} />
-          <div>
-              <h5>EMGT6305 Financial Management for Engrs</h5>
-              <p className="wd-dashboard-course-title"> Financial Management for Engineers </p>
-              <button> Go </button>
-          </div>
-          </Link>
-        </div> <br />
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link" to="/Kanbas/Courses/5100/Home">
-          <img src="/images/pd.jpg" width={200} />
-          <div>
-              <h5>GE5100 Product Developement for Engrs</h5>
-              <p className="wd-dashboard-course-title"> Product Developement for Engineers </p>
-              <button> Go </button>
-          </div>
-          </Link>
-        </div> <br />
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link" to="/Kanbas/Courses/6200/Home">
-          <img src="/images/ps.jpg" width={200} />
-          <div>
-              <h5>IE6200 Engineering Probs & Stats</h5>
-              <p className="wd-dashboard-course-title"> Engineering Probability & Statistics </p>
-              <button> Go </button>
-          </div>
-          </Link>
-        </div> <br />
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link" to="/Kanbas/Courses/6205/Home">
-          <img src="/images/or.jpg" width={200} />
-          <div>
-              <h5>OR6205 Deterministic Ops Research</h5>
-              <p className="wd-dashboard-course-title"> Deterministic Operations Research </p>
-              <button> Go </button>
-          </div>
-          </Link>
-        </div>
+      <div id="wd-dashboard-courses" className="row">
+      <div className="row row-cols-1 row-cols-md-5 g-4">
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card rounded-3 overflow-hidden">
+            <Link className="wd-dashboard-course-link text-decoration-none text-dark" to="/Kanbas/Courses/1234/Home">
+              <img src="/images/reactjs.jpg"  width="100%" height={160} />
+              <div  className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">CS1234 React JS</h5>
+                  <p className="wd-dashboard-course-title card-text"> Full Stack software developer </p>
+                  <button className="btn btn-primary"> Go </button>
+              </div>
+            </Link>
+          </div> 
+        </div> 
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card rounded-3 overflow-hidden">
+            <Link className="wd-dashboard-course-link text-decoration-none text-dark" to="/Kanbas/Courses/5610/Home">
+              <img src="/images/wd.jpg"  width="100%" height={160} />
+              <div  className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">CS5610 Web Development</h5>
+                  <p className="wd-dashboard-course-title card-text"> Web Application Development <br/> 202510_1 Fall 2025 Semester Full Term</p>
+                  <button className="btn btn-primary"> Go </button>
+              </div>
+            </Link>
+          </div> 
+        </div> 
+        
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card rounded-3 overflow-hidden">
+            <Link className="wd-dashboard-course-link text-decoration-none text-dark" to="/Kanbas/Courses/5520/Home">
+              <img src="/images/mad.jpg"  width="100%" height={160} />
+              <div  className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">CS5520 Mobile Application Development</h5>
+                  <p className="wd-dashboard-course-title card-text"> Mobile Application Development </p>
+                  <button className="btn btn-primary"> Go </button>
+              </div>
+            </Link>
+          </div> 
+        </div> 
+        
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card rounded-3 overflow-hidden">
+            <Link className="wd-dashboard-course-link text-decoration-none text-dark" to="/Kanbas/Courses/5220/Home">
+              <img src="/images/pm.jpg"  width="100%" height={160} />
+              <div  className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">EMGT5220 Engineering Project Management</h5>
+                  <p className="wd-dashboard-course-title card-text"> Engineering Project Management </p>
+                  <button className="btn btn-primary"> Go </button>
+              </div>
+            </Link>
+          </div> 
+        </div> 
+        
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card rounded-3 overflow-hidden">
+            <Link className="wd-dashboard-course-link text-decoration-none text-dark" to="/Kanbas/Courses/6305/Home">
+              <img src="/images/fm.jpg"  width="100%" height={160} />
+              <div  className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">EMGT6305 Financial Management for Engrs</h5>
+                  <p className="wd-dashboard-course-title card-text"> Financial Management for Engineers </p>
+                  <button className="btn btn-primary"> Go </button>
+              </div>
+            </Link>
+          </div> 
+        </div> 
+        
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card rounded-3 overflow-hidden">
+            <Link className="wd-dashboard-course-link text-decoration-none text-dark" to="/Kanbas/Courses/5100/Home">
+              <img src="/images/pd.jpg"  width="100%" height={160} />
+              <div  className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">GE5100 Product Developement for Engrs</h5>
+                  <p className="wd-dashboard-course-title card-text"> Product Developement for Engineers </p>
+                  <button className="btn btn-primary"> Go </button>
+              </div>
+            </Link>
+          </div> 
+        </div> 
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card rounded-3 overflow-hidden">
+            <Link className="wd-dashboard-course-link text-decoration-none text-dark" to="/Kanbas/Courses/6200/Home">
+              <img src="/images/ps.jpg"  width="100%" height={160} />
+              <div  className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">IE6200 Engineering Probs & Stats</h5>
+                  <p className="wd-dashboard-course-title card-text"> Engineering Probability & Statistics </p>
+                  <button className="btn btn-primary"> Go </button>
+              </div>
+            </Link>
+          </div> 
+        </div> 
+        
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card rounded-3 overflow-hidden">
+            <Link className="wd-dashboard-course-link text-decoration-none text-dark" to="/Kanbas/Courses/6205/Home">
+              <img src="/images/or.jpg"  width="100%" height={160} />
+              <div  className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">OR6205 Deterministic Ops Research</h5>
+                  <p className="wd-dashboard-course-title card-text"> Deterministic Operations Research </p>
+                  <button className="btn btn-primary"> Go </button>
+              </div>
+            </Link>
+          </div> 
+        </div> 
+      </div>
       </div>
     </div>
   );
--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,16 +1,51 @@
 import { Link } from "react-router-dom";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { MdOutlineMoveToInbox } from "react-icons/md";
+import { MdHome } from "react-icons/md";
+import { FaLaptopCode } from "react-icons/fa6";
+import { LiaBookSolid } from "react-icons/lia";
+import { FaRegCircleUser } from "react-icons/fa6";
+
 export default function KanbasNavigation() {
   return (
-    <div id="wd-kanbas-navigation">
-    <ul>
-      <li><a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/></li>
-      <li><Link to="/Kanbas/Account" id="wd-account-link">Account</Link><br/></li>
-      <li><Link to="/Kanbas/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/></li>
-      <li><Link to="/Kanbas/Dashboard" id="wd-course-link">Courses</Link><br/></li>
-      <li><Link to="/Kanbas/Calendar" id="wd-calendar-link">Calendar</Link><br/></li>
-      <li><Link to="/Kanbas/Inbox" id="wd-inbox-link">Inbox</Link><br/></li>
-      <li><Link to="/Labs" id="wd-labs-link">Labs</Link><br/></li>
-      <li><Link to="/LandingPage" id="wd-lp-link">LandingPage</Link><br/></li>
-    </ul>
+    <div id="wd-kanbas-navigation" style={{ width: 120 }} className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+      <a id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/" className="list-group-item bg-black border-0 text-center">
+      <img src="/images/NEU.png" width="75px" /></a>
+      <Link to="/Kanbas/Account" id="wd-account-link"
+        className="list-group-item text-center border-0 bg-black text-white">
+        <FaRegCircleUser className="fs-1 text text-white" /><br />
+        Account </Link>
+      <Link to="/Kanbas/Dashboard" id="wd-dashboard-link"
+        className="list-group-item text-center border-0
+                   bg-white text-danger">
+        <AiOutlineDashboard className="fs-1 text-danger" /><br />
+        Dashboard </Link>
+      <Link to="/Kanbas/Dashboard" id="wd-course-link"
+        className="list-group-item text-white
+                   bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        Courses </Link>
+      <Link to="/Kanbas/Calendar" id="wd-calendar-link"
+      className="list-group-item text-white
+                  bg-black text-center border-0">
+      <IoCalendarOutline className="fs-1 text-danger" /><br />
+      Calendar </Link>
+      <Link to="/Kanbas/Inbox" id="wd-inbox-link"
+      className="list-group-item text-white
+                  bg-black text-center border-0">
+      <MdOutlineMoveToInbox className="fs-1 text-danger" /><br />
+      Inbox </Link>
+      <Link to="/Labs" id="wd-labs-link"
+      className="list-group-item text-white
+                  bg-black text-center border-0">
+      <FaLaptopCode className="fs-1 text-danger" /><br />
+      Labs </Link>
+      <Link to="/LandingPage" id="wd-lp-link"
+      className="list-group-item text-white
+                  bg-black text-center border-0">
+      <MdHome className="fs-1 text-danger" /><br />
+      LandingPage </Link>
+    
     </div>
 );}
