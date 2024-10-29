6256f0b08359fabe5ff0c1cdee62d9a2873d1bea
PujithaSaiTulasi <mps.tulasi@gmail.com>
2024-10-28 20:46:44 -0400

    started a4

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,17 @@
+import ClickEvent from "./ClickEvent";
+
+export default function Lab4() {
+  
+    return (
+      <div id="wd-lab4" className="container">
+        <h3>Lab 4</h3>
+
+        <ClickEvent />
+
+        
+        
+       
+      </div>
+    );
+  }
+  
