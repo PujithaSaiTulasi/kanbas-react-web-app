dea54ce7860dc851308c9be0b98bbcea0c9f9a1f
PujithaSaiTulasi <mps.tulasi@gmail.com>
2024-10-31 21:39:27 -0400

    enrollements completed

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -2,10 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
 import accountReducer from "./Account/reducer";
 import assignmentsReducer from "./Courses/Assignments/reducer";
+import enrollmentsReducer from "./enrollmentreducer";
 
 const store = configureStore({
   reducer: {
-    modulesReducer, accountReducer, assignmentsReducer
+    modulesReducer, accountReducer, assignmentsReducer, enrollmentsReducer
   },
 });
 export default store;
