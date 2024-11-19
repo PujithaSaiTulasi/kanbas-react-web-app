387a691742a0435d7c316f695bf8f2737a96ca99
PujithaSaiTulasi <mps.tulasi@gmail.com>
2024-11-19 16:13:19 -0500

    lab5

--- src/LandingPage/TOC.tsx
+++ src/LandingPage/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
       <li><Link to="/Labs/Lab3">Lab 3</Link></li>
       <li><Link to="/Labs/Lab4">Lab 4</Link></li>
+      <li><Link to="/Labs/Lab5">Lab 5</Link></li>
       <li><Link to="/Kanbas">Kanbas</Link></li>
       <li><a id="wd-github" href="https://github.com/PujithaSaiTulasi/kanbas-react-web-app/tree/a4">Github</a></li>
     </ul>
