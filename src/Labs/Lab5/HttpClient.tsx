593c8a11e57cb70237811aef32602b5e8e22b531
PujithaSaiTulasi <mps.tulasi@gmail.com>
2024-11-23 01:42:48 -0500

    lab5 chnages

--- src/Labs/Lab5/HttpClient.tsx
+++ src/Labs/Lab5/HttpClient.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import * as client from "./client";
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
